a6505abcbf7a7d8a8e226e90c7c5cc67cbc0566b
TejasDev2026 <tejasghondage@gmail.com>
2025-03-25 18:04:32 +0000

    Comaprision

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,21 @@
+/*
+==
+===
+<
+>
+<=
+>=
+!=
+*/
+// following are bad practises of using javascript  it  was strictly prevented in typescript but if we avoid  this mistakes we can write a efficient and industry ready program
+// 1. comaprision beween two different datatypes Ex.
+console.log("1"==1) ;
+console.log("01"==1) ;
+console.log(1=="1") ;
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
