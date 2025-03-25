50923840272987e420ab29ca1363c5943ba6d1e2
TejasDev2026 <tejasghondage@gmail.com>
2025-03-25 14:25:57 +0000

    Datatypes

---
+++ 01_Basics/01Datatypes.js
@@ -0,0 +1,24 @@
+// number
+let phoneNumber=9356096145;
+console.table([typeof phoneNumber,phoneNumber]);
+// bigint  : use if number is greater than 2 to the power 52 
+
+// String 
+
+let nameOfAccountHolder="Tejas Ghondage";
+console.table([typeof nameOfAccountHolder,nameOfAccountHolder]);
+
+// boolean 
+
+let isUserLoggedIn=true;
+console.table([typeof isUserLoggedIn,isUserLoggedIn]);
+
+// null
+
+let currentTemperature=null;
+console.table([typeof currentTemperature,currentTemperature]);
+
+// undefined 
+
+let userNickName;
+console.log(typeof userNickName,userNickName);
