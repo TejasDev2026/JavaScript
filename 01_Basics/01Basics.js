93225b421f45f02297f40243a992d065846189b9
TejasDev2026 <tejasghondage@gmail.com>
2025-03-25 13:53:00 +0000

    Configuration settings done with sone intro about keywords and functions like log

---
+++ 01_Basics/01Basics.js
@@ -0,0 +1,13 @@
+"use strict";
+let x=190;
+var y=23;
+const name="Tejas";
+console.log(x);
+console.log(z);
+console.log(y);
+console.log(name);
+console.table([x,y,z,name]);
+console.warn("Tejas Do or Die");
+console.error('love you hubby');
+
+
